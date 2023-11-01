eabaff67511a9e06ae75ce1bf51aa7c3161ad821
harusvme <konovalov_denisd@mail.ru>
2023-11-01 22:02:58 +0400

    fix deploy

--- src/App.tsx
+++ src/App.tsx
@@ -1,17 +1,15 @@
-import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+import { Route, Routes } from "react-router-dom";
 import PostList from "./components/PostList/PostList";
 import PostDetail from "./components/PostDetail/PostDetail";
 
 const App: React.FC = () => {
   return (
-    <Router>
-      <div>
-        <Routes>
-          <Route path="/" element={<PostList />} />
-          <Route path="/post/:postId" element={<PostDetail />} />
-        </Routes>
-      </div>
-    </Router>
+    <div>
+      <Routes>
+        <Route path="/" element={<PostList />} />
+        <Route path="/post/:postId" element={<PostDetail />} />
+      </Routes>
+    </div>
   );
 };
 
--- src/main.tsx
+++ src/main.tsx
@@ -2,13 +2,16 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App.tsx";
 import { Provider } from "react-redux";
+import { HashRouter } from "react-router-dom";
 import store from "./store/store.ts";
 import "./index.css";
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
     <Provider store={store}>
-      <App />
+      <HashRouter>
+        <App />
+      </HashRouter>
     </Provider>
   </React.StrictMode>
 );
